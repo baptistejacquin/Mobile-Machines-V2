065abab9ad769a485961a8a18f3c1f35c5c1d59a
Baptiste Jacquin <baptiste.jacquin@Digital-Grenoble.local>
2018-03-06 11:47:57 +0100

    ajout des markeurs sur la map

--- src/MachineMap.vue
+++ src/MachineMap.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
       <gmap-map
-        :center="{lat:45.1841602, lng:5.750523}"
-        :zoom="13"
+        :center="{lat:10, lng:10}"
+        :zoom="7"
         style="width: 80%; height: 650px; margin: auto"
       >
+        <gmap-marker v-for="machine in machines"
+          :position="{lat:machine.latitude, lng:machine.longitude}"
+        ></gmap-marker>
       </gmap-map>
     </div>
 </template>
 
 <script>
     export default {
-        name: "machine-map"
+        name: "machine-map",
+      data: function () {
+        return {
+          machines: [{
+            id: 1,
+            latitude: 10,
+            longitude: 10,
+          },
+            {
+              id: 2,
+              latitude: 11,
+              longitude: 9.6,
+            }]
+        }
+
+      }
     }
 </script>
 
